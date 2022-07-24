270dc7c7dd107f938ebc41bf94fd260ea0f50d32
Gabriel Rodrigues <99913525+brittola@users.noreply.github.com>
2022-07-24 02:08:10 -0300

    update io emit

--- app.js
+++ app.js
@@ -11,18 +11,12 @@ const server = app.listen(PORT, () => {
     console.log('Server running on port ' + PORT);
 });
 
-const messages = [];
-
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
 
-    socket.emit('update_msgs', messages[messages.length - 1]);
-
     socket.on('new_msg', (data) => {
-        messages.push(data);
-
-        io.emit('update_msgs', messages);
+        io.emit('update_msgs', data);
     });
 
 });
